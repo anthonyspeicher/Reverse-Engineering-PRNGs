e9eb369f28c55d3b0928f56e5b456eeeb86b3b08
anthonyspeicher <anthony@speicher.org>
2025-12-19 14:55:43 -0500

    Created notes folder, updated gitignore, started state calculator testing

--- xorshift128+/state_calculator.cpp
+++ xorshift128+/state_calculator.cpp
@@ -12,14 +12,26 @@ using namespace std;
 
 int main() {
   /// initializing state and output list
-  xorshift128p_state outputs[4];
+  uint64_t outputs[4];
+  /// seed state
   xorshift128p_state state = {3, 4};
 
-  /// adds 4 values to outputs then shifts state once more - check state with function's result for 5th output
-  for (size_t i = 0; i < 4; i++) {
-    outputs[i] = state;
-    xorshift128p(state);
+  /// adds 4 return values to outputs
+  for (size_t i{0}; i < 4; i++) {
+    outputs[i] = xorshift128p(state);
   }
+
+  /// shift a 5th time to check with function's result for 5th output
+  xorshift128p(state);
+
+  xorshift128p_state test1 {7, 4};
+  xorshift128p_state test2 {3, 4};
+
+  xorshift128p(test1);
+  xorshift128p(test2);
+
+  cout << test1.x[0] << ", " << test1.x[1] << endl;
+  cout << test2.x[0] << ", " << test2.x[1] << endl;
 
   return 0;
 }
